f0f9fdcefd9e8b1de22ce0136871b5c69f32ad8c
chau nguyen <nguyenchau.baoloc@gmail.com>
2020-01-23 23:44:41 -0800

    picker 3

--- include/UWUEngine/Graphics/Debugs/Picker.h
+++ include/UWUEngine/Graphics/Debugs/Picker.h
@@ -14,6 +14,9 @@ public:
     static glm::vec3 GetMouseWorld();
     static void CalculateMouseWorld(glm::vec2 Pos);
 private:
+  static glm::mat3 GLFW_to_vp;
+  static glm::mat3 ndc_to_vp;
+  static glm::mat3 ndc_to_vf;
   static glm::vec3 mouse_world;
   static EntityID saved_ID;
   static float saved_t;
--- src/Graphics/Debugs/Picker.cpp
+++ src/Graphics/Debugs/Picker.cpp
@@ -10,6 +10,9 @@
 template<>
 int RegisterSystemHelper<Picker>::RegisterSystemHelper_ID = SystemUpdater::AddSystem<Picker>(SystemInitOrder::LAST, SystemUpdateOrder::Picker);
 
+glm::mat3 Picker::GLFW_to_vp;
+glm::mat3 Picker::ndc_to_vp;
+glm::mat3 Picker::ndc_to_vf;
 glm::vec3 Picker::mouse_world;
 EntityID Picker::saved_ID;
 float Picker::saved_t;
@@ -17,8 +20,44 @@ std::unordered_map<EntityID, float> Picker::ID_to_time_;
 
 Picker::Picker()
 {
-  saved_t = 1000.f;
+  saved_t = FLT_MAX;
   saved_ID = static_cast<unsigned>(-1);
+#pragma region convert glfw to viewport
+  GLFW_to_vp = glm::mat3(1.0f);
+  GLFW_to_vp[0].x = 1.0f;
+  GLFW_to_vp[1].y = -1.0f;
+  GLFW_to_vp[2].x = 0.0f;
+  GLFW_to_vp[2].y = WindowManager::getWindowHeight();
+  GLFW_to_vp[2].z = 1.0f;
+#pragma endregion
+
+#pragma region convert ndc to viewport
+  ndc_to_vp = glm::mat3(1.0f);
+  ndc_to_vp[0].x = WindowManager::getWindowWidth() / 2.0f;
+  ndc_to_vp[1].y = WindowManager::getWindowHeight() / 2.0f;
+  ndc_to_vp[2].x = WindowManager::getWindowWidth() / 2.0f + 0.0f;
+  ndc_to_vp[2].y = WindowManager::getWindowHeight() / 2.0f + 0.0f;
+  ndc_to_vp[2].z = 1.0f;
+#pragma endregion
+
+#pragma region convert ndc to view finder
+  float fov = Camera::getFOV();
+  float nearDist = Camera::getNearDistance();
+  float height_vf = 2.f * nearDist * tanf(glm::radians(fov) / 2.0f);
+  float aspectRatio = Camera::getAspectRatio();
+  float width_vf = aspectRatio * height_vf;
+
+  ndc_to_vf = glm::mat3(1.0f);
+  // asymmetric perspective
+  //ndc_to_vf[0].x = width_vf / 2.0f;
+  //ndc_to_vf[1].y = height_vf / 2.0f;
+  //ndc_to_vf[2].x = 0.0f;
+  //ndc_to_vf[2].y = 0.0f;
+  //ndc_to_vf[2].z = 1.0f;
+  // symmetric perspective
+  ndc_to_vf[0].x = 1.0f / tan(fov / 2.0f) / aspectRatio;
+  ndc_to_vf[1].y = 1.0f / tan(fov / 2.0f);
+#pragma endregion 
 }
 
 void Picker::Update()
@@ -52,7 +91,11 @@ void Picker::Update()
 
     pos = TransformComponentManager::GetTranslation(saved_ID);
     TraceLogger::Log(TraceLogger::DEBUG) << "after obj pos: x: " << pos.x <<
-      " y: " << pos.y << " z: " << pos.z << std::endl << std::endl;
+      " y: " << pos.y << " z: " << pos.z << std::endl;
+
+    auto cameraPos = Camera::GetCameraPosition();
+    TraceLogger::Log(TraceLogger::DEBUG) << "camera pos: x: " << cameraPos.x <<
+      " y: " << cameraPos.y << " z: " << cameraPos.z << std::endl << std::endl;
   }
 }
 
@@ -60,52 +103,16 @@ void Picker::CalculateMouseWorld(glm::vec2 Pos)
 {
   glm::vec3 mousePos = glm::vec3(Pos, 1.0f);
 
-#pragma region convert glfw to viewport
-  glm::mat3 GLFW_to_vp(1.0f);
-  GLFW_to_vp[0].x = 1.0f;
-  GLFW_to_vp[1].y = -1.0f;
-  GLFW_to_vp[2].x = 0.0f;
-  GLFW_to_vp[2].y = WindowManager::getWindowHeight();
-  GLFW_to_vp[2].z = 1.0f;
-#pragma endregion
-
-#pragma region convert viewport to ndc
-  glm::mat3 ndc_to_vp(1.0f);
-  ndc_to_vp[0].x = WindowManager::getWindowWidth() / 2.0f;
-  ndc_to_vp[1].y = WindowManager::getWindowHeight() / 2.0f;
-  ndc_to_vp[2].x = WindowManager::getWindowWidth() / 2.0f + 0.0f;
-  ndc_to_vp[2].y = WindowManager::getWindowHeight() / 2.0f + 0.0f;
-  ndc_to_vp[2].z = 1.0f;
-  glm::mat3 vp_to_ndc = glm::inverse(ndc_to_vp);
-#pragma endregion
-
-#pragma region convert ndc to view finder
-  float fov = Camera::getFOV();
-  float nearDist = Camera::getNearDistance();
-  float height_vf = 2.f * nearDist * tanf(glm::radians(fov) / 2.0f);
-  float aspectRatio = Camera::getAspectRatio();
-  float width_vf = aspectRatio * height_vf;
-
-  glm::mat3 ndc_to_vf(1.0f);
-  // asymmetric perspective
-  //ndc_to_vf[0].x = width_vf / 2.0f;
-  //ndc_to_vf[1].y = height_vf / 2.0f;
-  //ndc_to_vf[2].x = 0.0f;
-  //ndc_to_vf[2].y = 0.0f;
-  //ndc_to_vf[2].z = 1.0f;
-  // symmetric perspective
-  ndc_to_vf[0].x = 1.0f / tan(fov / 2.0f) / aspectRatio;
-  ndc_to_vf[1].y = 1.0f / tan(fov / 2.0f);
-#pragma endregion 
-
 #pragma region convert view finder to view frame
-  glm::vec3 P_vf = ndc_to_vf * vp_to_ndc * GLFW_to_vp * mousePos;
-  glm::vec4 P_view = { P_vf.x, P_vf.y, -1 * nearDist, 1.f };
+  glm::vec3 P_vf = ndc_to_vf * glm::inverse(ndc_to_vp) * GLFW_to_vp * mousePos;
+  glm::vec4 P_view = { P_vf.x, P_vf.y, -1 * Camera::getNearDistance(), 1.f };
 #pragma endregion 
 
 #pragma region convert view frame to world
-  glm::mat4 view_to_world = glm::inverse(Camera::GetViewMatrix());
-  mouse_world = glm::vec3(view_to_world * P_view);
+  glm::mat4 world_to_view = Camera::GetViewMatrix();
+  glm::mat4 view_to_world = glm::inverse(world_to_view);
+  glm::vec4 temp = view_to_world * P_view;
+  mouse_world = glm::vec3(temp);
 #pragma endregion 
 }
 
@@ -135,14 +142,17 @@ void Picker::Pick()
     glm::vec3 n = glm::normalize(glm::cross(v, u));
 
     // ray direction
-    glm::vec3 d = glm::normalize(mouse_world - cameraPos);
+    glm::vec3 ray_direction = glm::normalize(mouse_world - cameraPos);
 
-    float denom = glm::dot(d, n);
+    // distance from camera position to a plane
+    float distance = glm::dot(cameraPos - objectPos, n);
+
+    float denom = glm::dot(ray_direction, n);
     // plane and ray are not parallel
     if (denom > 1e-6)
     {
-      float numerator = glm::dot(glm::normalize(objectPos - cameraPos), n);
-      float t = numerator / denom;
+      float numerator = glm::dot(cameraPos, n) + distance;
+      float t = -1 * numerator / denom;
       if (t >= 0)
       {
         ID_to_time_[*it] = t;
