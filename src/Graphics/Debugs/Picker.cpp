270dd2f9082e0bb33e3d5e26915fa03c3ef46e6e
chaunguyen <chau.nguyen@digipen.edu>
2020-01-23 20:04:00 -0800

    picker 2

--- src/Graphics/Debugs/Picker.cpp
+++ src/Graphics/Debugs/Picker.cpp
@@ -4,6 +4,16 @@
 #include <UWUEngine/constants.h>
 #include <UWUEngine/WindowManager.h>
 #include <UWUEngine/Component/TransformComponentManager.h>
+#include "UWUEngine/Editor/Windows/EditorEntityViewer.h"
+#include "UWUEngine/Debugs/TraceLogger.h"
+
+template<>
+int RegisterSystemHelper<Picker>::RegisterSystemHelper_ID = SystemUpdater::AddSystem<Picker>(SystemInitOrder::LAST, SystemUpdateOrder::Picker);
+
+glm::vec3 Picker::mouse_world;
+EntityID Picker::saved_ID;
+float Picker::saved_t;
+std::unordered_map<EntityID, float> Picker::ID_to_time_;
 
 Picker::Picker()
 {
@@ -11,9 +21,44 @@ Picker::Picker()
   saved_ID = static_cast<unsigned>(-1);
 }
 
-EntityID Picker::Pick()
+void Picker::Update()
 {
-  glm::vec3 mousePos = glm::vec3(InputManager::GetMousePos(),1.0f);
+  
+  if (InputManager::MousePressed(InputConstants::Mouse::LEFT_CLICK))
+  {
+    glm::vec2 mousePos = InputManager::GetMousePos();
+    TraceLogger::Log(TraceLogger::DEBUG) << "Mouse GLFW: x: " << mousePos.x << " y: " <<
+      mousePos.y << std::endl;
+
+    CalculateMouseWorld(mousePos);
+    TraceLogger::Log(TraceLogger::DEBUG) << "Mouse World x: " << mouse_world.x << " y: " <<
+      mouse_world.y << " z: " << mouse_world.z << std::endl;
+
+    Pick();
+    PickID();
+    TraceLogger::Log(TraceLogger::DEBUG) << "chosen ID: " << saved_ID << std::endl;
+
+    Editors::EntityViewer::SetSelectedEntity(saved_ID);
+
+    auto pos = TransformComponentManager::GetTranslation(saved_ID);
+    TraceLogger::Log(TraceLogger::DEBUG) << "current obj pos: x: " << pos.x <<
+      " y: " << pos.y << " z: " << pos.z << std::endl;
+
+    TransformComponentManager::SetTranslation(
+      { mouse_world.x,
+        mouse_world.y,
+        pos.z,1.0f },
+      saved_ID);
+
+    pos = TransformComponentManager::GetTranslation(saved_ID);
+    TraceLogger::Log(TraceLogger::DEBUG) << "after obj pos: x: " << pos.x <<
+      " y: " << pos.y << " z: " << pos.z << std::endl << std::endl;
+  }
+}
+
+void Picker::CalculateMouseWorld(glm::vec2 Pos)
+{
+  glm::vec3 mousePos = glm::vec3(Pos, 1.0f);
 
 #pragma region convert glfw to viewport
   glm::mat3 GLFW_to_vp(1.0f);
@@ -55,9 +100,12 @@ EntityID Picker::Pick()
 
 #pragma region convert view frame to world
   glm::mat4 view_to_world = glm::inverse(Camera::GetViewMatrix());
-  glm::vec3 P_world = glm::vec3(view_to_world * P_view);
+  mouse_world = glm::vec3(view_to_world * P_view);
 #pragma endregion 
+}
 
+void Picker::Pick()
+{
   glm::vec3 cameraPos = Camera::GetCameraPosition();
 
   // get a list of current active objects
@@ -65,54 +113,54 @@ EntityID Picker::Pick()
 
   for (auto it = ids.begin(); it != ids.end(); ++it)
   {
+    // ray form: r(t) = cameraPos + t * (P_world - cameraPos) = cameraPos + t*d
     // get object current position in world coord
     glm::vec3 objectPos = glm::vec3(TransformComponentManager::GetTranslation(*it));
 
+    // scale factors of object
     glm::vec3 scale = TransformComponentManager::GetScale(*it);
 
-    float r = glm::max(glm::max(scale.x, scale.y), scale.z);
-    float r_square = r * r;
+    // calculate normal of the plane
+    glm::vec3 bottom_left = { objectPos.x - scale.x / 2.f, objectPos.y - scale.y / 2.f, objectPos.z };
+    glm::vec3 top_left = { objectPos.x - scale.x / 2.f, objectPos.y + scale.y / 2.f, objectPos.z };
+    glm::vec3 bottom_right = { objectPos.x + scale.x / 2.f, objectPos.y - scale.y / 2.f, objectPos.z };
 
-    // ray form: r(t) = cameraPos + t * (P_world - cameraPos)
-    glm::vec3 q = P_world - cameraPos;            // P_W - E_W
-    glm::vec3 cam_to_obj = objectPos - cameraPos; // C_W - E_W
-    glm::vec3 mouse_to_obj = objectPos - P_world; // C_W - P_W
-    float p = glm::dot(objectPos - cameraPos,q);
-    float m = p / q.length();
-    float len = cam_to_obj.length();
-    float n_square = cam_to_obj.length() * cam_to_obj.length() - (m * m);
+    glm::vec3 u = top_left - bottom_left;
+    glm::vec3 v = bottom_right - bottom_left;
+    glm::vec3 n = glm::normalize(glm::cross(v, u));
 
-    if (glm::dot(cam_to_obj, cam_to_obj) <= r_square &&
-        glm::dot(mouse_to_obj, mouse_to_obj) <= r_square)
+    // ray direction
+    glm::vec3 d = glm::normalize(mouse_world - cameraPos);
+
+    float denom = glm::dot(d, n);
+    // plane and ray are not parallel
+    if (denom > 1e-6)
     {
-      // trivial acceptance
-      return *it;
-    }
-
-    // camera is outside of sphere
-    if (glm::dot(cam_to_obj, cam_to_obj) > r_square)
-    {
-      // ray is moving away from sphere
-      if (p < 0)
+      float numerator = glm::dot(glm::normalize(objectPos - cameraPos), n);
+      float t = numerator / denom;
+      if (t >= 0)
       {
-        continue;
-      }
-      // ray is moving toward sphere but will miss it
-      if (m > 0 && n_square > r_square)
-      {
-        continue;
-      }
-      // ray intersects with sphere
-      float s = sqrtf(r_square - n_square);
-      float t_e = m - s;
-      float t = t_e / q.length();
-      if (t < saved_t)
-      {
-        saved_t = t;
-        saved_ID = *it;
+        ID_to_time_[*it] = t;
       }
     }
   }
-
-  return saved_ID;
 }
+
+void Picker::PickID()
+{
+  for (auto it = ID_to_time_.begin(); it != ID_to_time_.end(); ++it)
+  {
+    if (it->second < saved_t)
+    {
+      saved_t = it->second;
+      saved_ID = it->first;
+    }
+  }
+}
+
+glm::vec3 Picker::GetMouseWorld()
+{
+  return mouse_world;
+}
+
+
